57adae01c3dcb552f53e7665faf3325c2495c844
Nythero <agustin-ag@live.com>
2021-11-13 12:33:12 -0300

    Added MYSQLPORT to pool connection and in .env

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise');
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.DATABASE
